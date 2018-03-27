ed2b83ca65d231855dc63a858f6d90b4e9d45bb7
Jason Ioannides <jason.ioannides@gmail.com>
2018-03-27 09:24:49 -0400

    first deployed copy

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize')
+
+console.log(process.env.DATABASE_URL, process.env.PASSWORD, process.env.USERNAME)
 const db = new Sequelize(
-    'postgres://localhost:5432/apitest',
-    {
+    process.env.DATABASE_URL,
+    {   
+        dialect: 'postgres',
         logging: false
     }
 );
+
+
 
 module.exports = db;

917b731ea60db2d0a731701294dbcf2b8e5901d7
Jason Ioannides <jason.ioannides@gmail.com>
2018-07-02 17:43:09 -0400

    minor changes

--- server/api/soap/index.js
+++ server/api/soap/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
     res.type('application/xml');
-    res.send('<top_level_bracket> <outer_bracket> <test> test </test> </outer_bracket> </top_level_bracket>')
+    res.send('<?xml version="1.0" encoding="utf-8"?>  <Envelope> <Body> <Message id="messageIdentity"> test </Message> </Body> </Envelope>')
 })
 
 

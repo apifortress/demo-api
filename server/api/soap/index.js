45239beafc71acab41978e3520506fd9856490dc
Jason Ioannides <jason.ioannides@gmail.com>
2018-07-03 16:20:58 -0400

    proper SOAP formatting in SOAP endpoint

--- server/api/soap/index.js
+++ server/api/soap/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
     res.type('application/xml');
-    res.send('<?xml version="1.0" encoding="utf-8"?>  <Envelope> <Body> <Message id="messageIdentity"> test </Message> </Body> </Envelope>')
+    res.send('<?xml version="1.0"?> <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope/" soap:encodingStyle="http://www.w3.org/2003/05/soap-encoding"> <soap:Body> <m:GetPriceResponse xmlns:m="https://www.w3schools.com/prices"> <m:Price>1.90</m:Price> </m:GetPriceResponse> </soap:Body> </soap:Envelope>')
 })
 
 
 module.exports = router
+

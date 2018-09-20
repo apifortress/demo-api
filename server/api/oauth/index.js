591f2ce1ef5bbf351c70504028729cc544c9f67e
Jason Ioannides <jason.ioannides@gmail.com>
2018-09-20 13:24:44 -0400

    Fixed blank header auth issue

--- server/api/oauth/index.js
+++ server/api/oauth/index.js
@@ -2,28 +2,36 @@ const router = require('express').Router()
 const User = require('../../db/models/user')
 
 router.post('/auth', (req, res, next) => {
-    const authHeader = req.headers.authorization.split(' ')
-    if (authHeader[0] === "Basic") {
-        const userPass = (Buffer.from(authHeader[1], 'base64').toString())
-        const username = userPass.split(":")[0]
-        const password = userPass.split(":")[1]
-        User.findOne({
-            where: {
-                userName: username
-            }
-        }).then(
-            (user) => {
-                if (password == user.password){
-                    res.send({message: "Authentication Successful", Token: "USERTOKEN123AAABBB"})
-                } else {
-                    res.status(401)
-                    res.send("Bad Username/Password")
-                }
-            } 
-        )
-    } else {
+    if (req.headers.authorization === undefined) {
         res.status(401)
         res.send("Bad Authorization Header")
+    } else {
+        const authHeader = req.headers.authorization.split(' ')
+        if (authHeader[0] === "Basic") {
+            const userPass = (Buffer.from(authHeader[1], 'base64').toString())
+            const username = userPass.split(":")[0]
+            const password = userPass.split(":")[1]
+            User.findOne({
+                where: {
+                    userName: username
+                }
+            }).then(
+                (user) => {
+                    if (user === null) {
+                        res.status(401)
+                        res.send("Bad Username/Password")
+                    } else if (password == user.password){
+                        res.send({message: "Authentication Successful", Token: "USERTOKEN123AAABBB"})
+                    } else {
+                        res.status(401)
+                        res.send("Bad Username/Password")
+                    }
+                } 
+            )
+        } else {
+            res.status(401)
+            res.send("Bad Authorization Header")
+        }
     }
 })
 
